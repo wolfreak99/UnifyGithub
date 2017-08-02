806d6e97434607fdab6270d85c079ff58923a75f
wolfreak99 <jbminor1991@gmail.com>
2017-08-02 19:35:42 -0400

    Format General/CodeSnippets/AbortableEnumerator

--- Scripts/General/CodeSnippets/AbortableEnumerator.cs
+++ Scripts/General/CodeSnippets/AbortableEnumerator.cs
@@ -3,14 +3,21 @@
  * Github url: https://github.com/wolfreak99/UnifyGithub/blob/master/Scripts/General/CodeSnippets/AbortableEnumerator.cs
  * File based on original modification date of: 25 December 2013, at 23:03. 
  *
+ * Description
+ *   Provides an abort mechanism to a coroutine. 
+ *   Wrap the call to the coroutine with this AbortableEnumerator, and call Abort() when you want to abort it. 
+ * Usage
+ *   See this answer for usage example
+ *   
  * This file has not yet been properly formatted, feel free to contribute!
  *
  *************************/
+// TODO when site server is fixed, retrieve link for Usage's "See this answer" context
 
 namespace UnifyGithub.General.CodeSnippets
 {
-    Provides an abort mechanism to a coroutine. Wrap the call to the coroutine with this AbortableEnumerator, and call Abort() when you want to abort it. 
-    See this answer for usage example. 
+    using System.Collections;
+
     public class AbortableEnumerator : IEnumerator
     {
     	protected IEnumerator enumerator;
