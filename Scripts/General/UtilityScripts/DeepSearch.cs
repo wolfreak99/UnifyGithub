025e6a2ba951afa68f34d2d56b30e0d9b53181f1
wolfreak99 <jbminor1991@gmail.com>
2017-08-03 11:40:04 -0400

    Format: General/UtilityScripts/DeepSearch.cs

--- Scripts/General/UtilityScripts/DeepSearch.cs
+++ Scripts/General/UtilityScripts/DeepSearch.cs
@@ -3,43 +3,30 @@
  * Github url: https://github.com/wolfreak99/UnifyGithub/blob/master/Scripts/General/UtilityScripts/DeepSearch.cs
  * File based on original modification date of: 10 January 2012, at 20:52. 
  *
- * This file has not yet been properly formatted, feel free to contribute!
+ * This article is a stub. 
+ * You can help UnifyWiki by expanding it. 
  *
  *************************/
 
 namespace UnifyGithub.General.UtilityScripts
 {
-    This article is a stub. 
-    You can help UnifyWiki by expanding it. 
-    
-    JS Scriptstatic function deepSearch (t : Transform, s : String) : Transform
+    using UnityEngine;
+
+    public class DeepSearch
     {
-    	var	dt : Transform = t.Find (s);
-    	if (dt)	
-    		return dt;
-    	else
-    	{
-    		for (var child : Transform in t) {
-    			dt = deepSearch (child, s);
-    			if (dt) 
-    				return dt;
-    		}
-    	}	
-    	return null;
-    }C# Script   static public Transform deepSearch(Transform t, string s)
-       {
-           Transform dt = t.Find(s);
-           if (dt != null)
-               return dt;
-           else
-           {
-               foreach (Transform child in t)
-               {
-                   dt = deepSearch(child, s);
-                   if (dt != null)
-                       return dt;
-               }
-           }
-           return null;
-       }
+        static public Transform deepSearch(Transform t, string s)
+        {
+            Transform dt = t.Find(s);
+            if (dt != null)
+                return dt;
+            else {
+                foreach (Transform child in t) {
+                    dt = deepSearch(child, s);
+                    if (dt != null)
+                        return dt;
+                }
+            }
+            return null;
+        }
+    }
 }
